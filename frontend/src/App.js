e58e5585f29190f236781026d7de1151bef4006e
jacobogonzalezdiaz <jacobo_gonzalez@msn.com>
2023-07-28 20:07:52 +0100

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>AMAZONA</a>
+      </header>
+      <main>LISTA DE PRODUCTOS</main>
+    </div>
+  );
+}
+
+export default App;
