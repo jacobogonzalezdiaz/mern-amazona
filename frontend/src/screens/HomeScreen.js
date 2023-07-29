9a390dc4c01fbdb2f15defd913ef4efcad353d4d
jacobogonzalezdiaz <jacobo_gonzalez@msn.com>
2023-07-29 15:22:09 +0100

    video-8

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom";
-import data from "../data";
+//import data from "../data";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function HomeScreen (){
+  const[products, seProducts]= useState([])
+  useEffect(()=>{
+    const fetchData = async()=>{
+      const result = await axios.get('/api/products');
+      seProducts(result.data);
+    };
+    fetchData();
+  }, [])
     return <div>
         <h1>Productos Destacados</h1>
           <div className='products'>
-            {data.products.map((product) => (
+            {products.map((product) => (
               <div className='product' key={product.slug}>
                 <Link to={`/product/${product.slug}`}>
                   <img src={product.image} alt={product.name} />
